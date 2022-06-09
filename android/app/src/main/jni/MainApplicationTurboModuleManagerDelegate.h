1fa38d44139bfd2e19fe36c28b945a140d9cfef0
Joseph Hale <me@jhale.dev>
2022-06-08 22:07:54 -0700

    Update Android src path to `org.speedcuber.timer`
    The src path was still at `com/speedcubertimer` as auto-generated by the
    react-native template. This commit updates those paths.

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/speedcubertimer/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lorg/speedcuber/timer/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/speedcubertimer/newarchitecture/components/MainComponentsRegistry;";
+      "Lorg/speedcuber/timer/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
