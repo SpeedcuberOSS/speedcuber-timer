19c02ab19528c82d4eebb12922b848725bac1eff
Joseph Hale <me@jhale.dev>
2022-11-03 12:29:37 -0700

    feat: Add visualization of last move
    Reading JSON can be difficult, so I've added a small box and icon to
    visualize the most recently applied move on the smartcube more easily.

--- src/ui/screens/PlayScreen.tsx
+++ src/ui/screens/PlayScreen.tsx
@@ -4,11 +4,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Button, ProgressBar, Text } from 'react-native-paper';
+import { Button, IconButton, ProgressBar, Text } from 'react-native-paper';
 import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 import { DEFAULT_BLUETOOTH_SCAN_DURATION } from '../utils/bluetooth';
-import { SafeAreaView } from 'react-native';
+import Icons from '../icons/iconHelper';
 import SmartPuzzleConnector from '../components/SmartPuzzleConnector';
 import useSmartPuzzles from '../utils/bluetooth/useSmartPuzzles';
 import { useTranslation } from 'react-i18next';
@@ -16,7 +17,7 @@ import { useTranslation } from 'react-i18next';
 export default function PlayScreen() {
   const { puzzles, puzzleScan } = useSmartPuzzles();
   const [progressVisible, setProgressVisible] = useState(false);
-  const [moves, setMoves] = useState<string[]>([]);
+  const [lastMove, setLastMove] = useState<object>({});
   const { t } = useTranslation();
   return (
     <SafeAreaView>
@@ -43,10 +44,35 @@ export default function PlayScreen() {
           <SmartPuzzleConnector
             key={cube.id()}
             smartPuzzle={cube}
-            onMove={move => setMoves([...moves, move])}
+            onMove={move => setLastMove(JSON.parse(move))}
           />
         ))}
-      <Text>{moves.join(', ')}</Text>
+      <Text>{JSON.stringify(lastMove)}</Text>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: lastMove?.face?.toLowerCase() ?? 'black',
+        }}></View>
+      {lastMove.direction === 'clockwise' && (
+        <IconButton icon={Icons.MaterialIcons('rotate-right')} size={50} />
+      )}
+      {lastMove.direction === 'counterclockwise' && (
+        <IconButton icon={Icons.MaterialIcons('rotate-left')} size={50} />
+      )}
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    marginRight: 15,
+  },
+  connecting: {
+    marginHorizontal: 28,
+  },
+  connection: {
+    marginHorizontal: 20,
+    fontSize: 50,
+  },
+});
