24650fa8b596aae38a3664e549d450a960b88967
Joseph Hale <me@jhale.dev>
2022-11-09 14:58:37 -0700

    feat: POC Enable animating puzzles with cubing.js
    Cubing.js is a web-based library, so it only runs well in a WebView.
    However, WebViews can't easily communicate with the React Native engine,
    so we have to create some workarounds to proxy messages back and forth.
    
    In this commit, I added a hidden button to the WebView rendering the
    cubing.js `twisty-player`. When clicked, that hidden button add its
    current button text to the `twisty-player` as a new move.
    
    The React Native pieces then post JS messages to change the button text,
    then programmatically click it to add the move to the player.
    
    I tried to directly invoke the `player` object, but apparently it's out
    of scope for the `injectJavascript` calls. Thus the need for a reactive
    workaround.

--- src/ui/screens/LearnScreen.tsx
+++ src/ui/screens/LearnScreen.tsx
@@ -4,18 +4,61 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import React from 'react';
+import React, { useRef } from 'react';
+
 import { WebView } from 'react-native-webview';
 
 export default function LearnScreen() {
+  const webViewRef = useRef({});
+  const width = 384;
+  const height = 256;
+  const scale = 2;
+
+  function sendMove(move: string) {
+    const js = `
+      document.getElementById("next-move").innerHTML = "${move}"
+      document.getElementById("next-move").click()
+      true;
+    `;
+    console.log(js);
+    webViewRef.current.injectJavaScript(js);
+  }
+
+  setTimeout(() => {
+    sendMove('U');
+  }, 1000);
   return (
     <WebView
+      ref={webViewRef}
       originWhitelist={['*']}
       style={{ flex: 1 }}
       source={{
         html: `
-        <script src="https://cdn.cubing.net/js/cubing/twisty" type="module"></script>
-        <twisty-player alg="R U R' U''"></twisty-player>
+        <script type="module">
+          import { TwistyPlayer } from "https://cdn.cubing.net/js/cubing/twisty";
+          import { Move } from "https://cdn.cubing.net/js/cubing/alg";
+
+          const player = new TwistyPlayer({
+            puzzle: "3x3x3",
+            alg: "",
+            hintFacelets: "none",
+            backView: "none",
+            background: "none",
+            controlPanel: "none",
+          });
+          player.style = "width: ${width * scale}; height: ${height * scale};"
+          document.body.appendChild(player);
+          document.player = player
+          document.getElementById("next-move").addEventListener("click", () => {
+            var move = document.getElementById("next-move").innerHTML
+            player.experimentalAddAlgLeaf(new Move(move));
+          })
+        </script>
+
+        <body
+          style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; background-color: black">
+          <button hidden id="next-move">U</button>
+        </body>
       `,
       }}
     />
