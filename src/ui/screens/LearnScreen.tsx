b515665e227e1d3f925b8b4073c07495619cb5d1
Joseph Hale <me@jhale.dev>
2022-11-09 12:57:23 -0700

    feat: Render cubing.js from CDN in a WebView

--- src/ui/screens/LearnScreen.tsx
+++ src/ui/screens/LearnScreen.tsx
@@ -4,62 +4,20 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { FAB } from 'react-native-paper';
-import React, { useState } from 'react';
-
-// import StoryBook from '../../../storybook';
-import Icons from '../icons/iconHelper';
-
-function OpenStorybookFAB({ onPress }: { onPress: () => void }) {
-  return (
-    <FAB
-      style={styles.fabOpen}
-      icon={Icons.MaterialCommunityIcons('book')}
-      onPress={onPress}
-    />
-  );
-}
-
-function CloseStorybookFAB({ onPress }: { onPress: () => void }) {
-  return (
-    <FAB
-      style={styles.fabClosed}
-      icon={Icons.Entypo('open-book')}
-      onPress={onPress}
-    />
-  );
-}
+import React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function LearnScreen() {
-  const [showStorybook, setShowStorybook] = useState(false);
-  const onPressFAB = () => setShowStorybook(!showStorybook);
   return (
-    <SafeAreaView style={styles.container}>
-      {/* {showStorybook && <StoryBook />} */}
-      {showStorybook ? (
-        <CloseStorybookFAB onPress={onPressFAB} />
-      ) : (
-        <OpenStorybookFAB onPress={onPressFAB} />
-      )}
-    </SafeAreaView>
+    <WebView
+      originWhitelist={['*']}
+      style={{ flex: 1 }}
+      source={{
+        html: `
+        <script src="https://cdn.cubing.net/js/cubing/twisty" type="module"></script>
+        <twisty-player alg="R U R' U''"></twisty-player>
+      `,
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  fabOpen: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 2,
-  },
-  fabClosed: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 40,
-  },
-});
