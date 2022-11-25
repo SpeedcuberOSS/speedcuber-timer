bee9fbcdbdf079d616fd53ec3d55aa91c5e08bb6
Joseph Hale <me@jhale.dev>
2022-11-24 22:40:34 -0700

    feat: Create `FileSystemAttemptLibrary`
    Stores all attempts directly on the file system, which has much larger
    storage capacity than AsyncStorage.
    
    Currently, the full library is serialized and de-serialized for each new
    Attempt, which will likely become a performance bottleneck after a few
    thousand solves. But, for now, it is a sufficient upgrade.
    
    closes #68
    ^- This issue describes much more functionality than this commit
    implements, but until performance bottlenecks force our hand to improve
    the file system storage, I'm considering this issue closed.

--- src/lib/attempts/FileSystemAttemptLibrary.ts
+++ src/lib/attempts/FileSystemAttemptLibrary.ts
@@ -3,29 +3,41 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import { DocumentDirectoryPath, readFile, writeFile } from 'react-native-fs';
 import { logError, mergeLibraries, parseAttemptMap } from './_helpers';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { PersistentAttemptLibrary } from './PersistentAttemptLibrary';
 
-const LIBRARY_KEY = '@attempts';
+const STORAGE_PATH = `${DocumentDirectoryPath}/attempts.json`;
 
-class AsyncStorageAttemptLibrary extends PersistentAttemptLibrary {
+class FileSystemAttemptLibrary extends PersistentAttemptLibrary {
+  protected async loadLibraryFromStorage(): Promise<string> {
+    let storedJSONstr = '[]';
+    try {
+      storedJSONstr = await readFile(STORAGE_PATH);
+    } catch (error) {
+      logError('Failed to read attempts from storage')(error as Error);
+    }
+    return storedJSONstr;
+  }
   protected async persistChanges() {
-    const storedJSONstr = await AsyncStorage.getItem(LIBRARY_KEY);
+    const storedJSONstr = await this.loadLibraryFromStorage();
     const storedLibrary = parseAttemptMap(storedJSONstr);
     this._library = mergeLibraries(storedLibrary, this._library);
     const updatedLibraryString = JSON.stringify(Array.from(this._library));
-    AsyncStorage.setItem(LIBRARY_KEY, updatedLibraryString)
+    writeFile(STORAGE_PATH, updatedLibraryString)
       .then(_result =>
         console.log(
           `Successfully stored the updated library with ${this._library.size} attempts`,
         ),
       )
       .catch(
-        logError('Could not persist updated Attempt Library to AsyncStorage'),
+        logError(
+          'Could not persist updated Attempt Library to the File System',
+        ),
       );
   }
 }
 
-export { AsyncStorageAttemptLibrary };
+export { FileSystemAttemptLibrary };
