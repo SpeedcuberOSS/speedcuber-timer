d25d0b77abc694e19e9141f2417c8f4057673976
Joseph Hale <me@jhale.dev>
2023-01-22 00:45:01 -0700

    fix: Add theme to Storybook renderings

--- storybook/index.js
+++ storybook/index.js
@@ -9,7 +9,9 @@ import {
   getStorybookUI,
 } from '@storybook/react-native';
 
+import { Provider as PaperProvider } from 'react-native-paper';
 import React from 'react';
+import { getCurrentTheme } from '../src/ui/themes';
 import { loadStories } from './storyLoader';
 import { withKnobs } from '@storybook/addon-knobs';
 
@@ -24,7 +26,11 @@ configure(() => {
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({ asyncStorage: null });
-const Storybook = () => <StorybookUIRoot />;
+const Storybook = () => (
+  <PaperProvider theme={getCurrentTheme()}>
+    <StorybookUIRoot />
+  </PaperProvider>
+);
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
