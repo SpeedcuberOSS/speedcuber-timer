c0bcb228fe268974f1c0f6c8ff1dd6da1e1aeb24
Joseph Hale <me@jhale.dev>
2023-02-07 11:24:30 -0700

    feat: Add current AoX averages to `InsightsScreen`
    The global averages can also be computed, but performance is dreadful
    for large Attempt libraries so I won't include it in the app until
    performance improvements are put in place.

--- src/ui/screens/InsightsScreen.tsx
+++ src/ui/screens/InsightsScreen.tsx
@@ -4,9 +4,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 
+import { AttemptAnalytics } from '../../lib/analytics/AttemptAnalytics';
 import AttemptsChart from '../components/charts/AttemptsChart';
+import { DataTable } from 'react-native-paper';
 import React from 'react';
 import { TimerTabScreenProps } from '../navigation/types';
 import { getLibrary } from '../../lib/attempts';
@@ -23,9 +25,31 @@ export default function InsightsScreen(props: Props) {
     .filter(a => a.event === event)
     .sort((a, b) => b.unixTimestamp - a.unixTimestamp)
     .slice(0, 100);
+  const averages = [5, 12, 50, 100, 1000];
   return (
     <SafeAreaView style={styles.container}>
       <AttemptsChart attempts={attempts} />
+      <View style={{ padding: 20 }}>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Type</DataTable.Title>
+            {/* <DataTable.Title>Global</DataTable.Title> */}
+            <DataTable.Title>Current</DataTable.Title>
+          </DataTable.Header>
+        </DataTable>
+        {averages.map(count => (
+          <DataTable.Row key={count}>
+            <DataTable.Cell>{`Ao${count}`}</DataTable.Cell>
+            {/* <DataTable.Cell>
+              {Math.min(...new AttemptAnalytics(data).sliding.AoX(count)) /
+                1000}
+            </DataTable.Cell> */}
+            <DataTable.Cell>
+              {new AttemptAnalytics(data.slice(-count)).AoX(count) / 1000}
+            </DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </View>
     </SafeAreaView>
   );
 }
