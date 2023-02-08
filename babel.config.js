98fa2c28de37a70883f88b1e951818d88f05263c
Joseph Hale <me@jhale.dev>
2023-02-08 12:29:01 -0700

    build: Add extra checks to build/bundling
    rnx-kit will check for TypeScript violations, duplicate packages, and
    cyclical dependencies when building packages, including in development
    mode.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@rnx-kit/babel-preset-metro-react-native',
+  ],
   plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
   env: {
     production: {
