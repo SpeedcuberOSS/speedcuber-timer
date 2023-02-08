84cd9750ec5c1dd376b0ef03210eaf58880211d8
Joseph Hale <me@jhale.dev>
2023-02-08 10:32:02 -0700

    perf: Add performance optimizations for release builds
    Based on the recommendations from https://reactnative.dev/docs/performance

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
