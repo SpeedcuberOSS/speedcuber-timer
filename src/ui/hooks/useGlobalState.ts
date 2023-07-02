93d8371b221ae70f7c5b947dfd7aed323dc78490
Joseph Hale <me@jhale.dev>
2023-07-02 00:16:33 -0700

    refactor: Propogate STIF updates to `useGlobalState`

--- src/ui/hooks/useGlobalState.ts
+++ src/ui/hooks/useGlobalState.ts
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { EVENT_3x3x3 } from '../../lib/stif';
+import { EVENT_3x3x3 } from '../../lib/stif/builtins';
 import { createGlobalState } from 'react-hooks-global-state';
 
 const { useGlobalState } = createGlobalState({
