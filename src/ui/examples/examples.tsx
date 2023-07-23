f7355101f80e63d8cbed8bed0a29e93760de2f62
Joseph Hale <me@jhale.dev>
2023-07-23 00:04:28 -0700

    feat: Re-enable Examples for `CenteredBetweenSidebars`

--- src/ui/examples/examples.tsx
+++ src/ui/examples/examples.tsx
@@ -20,7 +20,7 @@ const examples: Map<string, DevelopmentExampleSet> = new Map();
   require('../components/PlayerControls/PlayerControls.examples').default,
   // require('../components/TwistyPlayer/AttemptPlayer.examples').default,
   require('../components/TwistyPlayer/TwistyPlayer.examples').default,
-  // require('../structure/CenteredBetweenSidebars.examples').default,
+  require('../structure/CenteredBetweenSidebars.examples').default,
 ].forEach(e => examples.set(e.key, e));
 
 /**
