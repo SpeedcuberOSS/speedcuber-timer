9a528d50e345f663d30ca8db79e3fb06d846aeaf
Joseph Hale <me@jhale.dev>
2023-07-22 23:21:47 -0700

    refactor: Propogate STIF updates to `EventSelector`
    The biggest change is the switch from a three column list to a single
    column list. This design does result in a taller list (i.e. more
    scrolling), but makes the event names fully readable and provides
    real-estate for additional interactivity (e.g. as used for the 333mbf
    event).

--- src/ui/components/events/EventSelector.tsx
+++ src/ui/components/events/EventSelector.tsx
@@ -4,66 +4,103 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Avatar, Text } from 'react-native-paper';
-import {
-  Dimensions,
-  FlatList,
-  Pressable,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { Divider, List } from 'react-native-paper';
+import { ScrollView } from 'react-native';
 
-import { CompetitiveEvent } from '../../../lib/stif';
+import { STIF } from '../../../lib/stif';
+import * as Events from '../../../lib/stif/builtins/CompetitiveEvents';
 import Icons from '../../icons/iconHelper';
-import { getKnownCompetitiveEvents } from '../../utils/knownEvents';
 import { useTranslation } from 'react-i18next';
+import Ticker from '../Ticker';
+import { Fragment, useCallback, useState } from 'react';
+import { PUZZLE_3x3x3 } from '../../../lib/stif/builtins';
 
 interface EventSelectorProps {
-  onSelect: (event: CompetitiveEvent) => void;
+  onSelect: (event: STIF.CompetitiveEvent) => void;
 }
 
 interface EventItemProps {
-  item: CompetitiveEvent;
-  onPress: () => void;
+  event: STIF.CompetitiveEvent;
+  onSelect: (event: STIF.CompetitiveEvent) => void;
 }
 
-function EventItem({ item, onPress }: EventItemProps) {
+function EventItem({ event, onSelect }: EventItemProps) {
   const { t } = useTranslation();
+  const [mbfCount, setMbfCount] = useState(2);
+  const pressHandler = useCallback(() => {
+    if (event.id === Events.EVENT_3x3x3_BLD_MULTI.id) {
+      onSelect({ ...event, puzzles: new Array(mbfCount).fill(PUZZLE_3x3x3) });
+    } else {
+      onSelect(event);
+    }
+  }, [event, onSelect, mbfCount]);
+  const Icon =
+    event.type === 'unofficial' ? Icons.WCAEventUnofficial : Icons.WCAEvent;
+
   return (
-    <Pressable style={styles.item} onPress={onPress}>
-      <Avatar.Icon icon={Icons.WCAEvent(item.id)} size={60} />
-      <Text numberOfLines={1} variant="labelSmall">
-        {t(`events.${item.id}`)}
-      </Text>
-    </Pressable>
+    <List.Item
+      title={t(`events.${event.id}`)}
+      onPress={pressHandler}
+      left={props => <List.Icon {...props} icon={Icon(event.id)} />}
+      right={props =>
+        event.id === '333mbf' ? (
+          <Ticker
+            initialValue={2}
+            min={2}
+            step={1}
+            onChange={value => setMbfCount(value)}
+            orientation="horizontal"
+          />
+        ) : null
+      }
+    />
   );
 }
 
 export default function EventSelector({ onSelect }: EventSelectorProps) {
+  const { t } = useTranslation();
+  const unsupportedEvents = ['unknown', '333bf-team', '23relay'];
+  const events = Object.values(Events).filter(
+    e => !unsupportedEvents.includes(e.id),
+  );
   return (
-    <View style={styles.container}>
-      <FlatList
-        columnWrapperStyle={styles.list}
-        data={getKnownCompetitiveEvents()}
-        numColumns={3}
-        renderItem={({ item }) => (
-          <EventItem key={item.id} item={item} onPress={() => onSelect(item)} />
-        )}
-      />
-    </View>
+    <ScrollView>
+      <List.Section>
+        <List.Subheader>{t('event.type.official')}</List.Subheader>
+        {events
+          .filter(e => e.type === 'official')
+          .map((e, idx) => (
+            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
+            <Fragment key={e.id}>
+              {!!idx && <Divider horizontalInset />}
+              <EventItem key={e.id} event={e} onSelect={onSelect} />
+            </Fragment>
+          ))}
+      </List.Section>
+      <List.Section>
+        <List.Subheader>{t('event.type.unofficial')}</List.Subheader>
+        {events
+          .filter(e => e.type === 'unofficial')
+          .map((e, idx) => (
+            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
+            <Fragment key={e.id}>
+              {!!idx && <Divider horizontalInset />}
+              <EventItem key={e.id} event={e} onSelect={onSelect} />
+            </Fragment>
+          ))}
+      </List.Section>
+      <List.Section>
+        <List.Subheader>{t('event.type.retired')}</List.Subheader>
+        {events
+          .filter(e => e.type === 'retired')
+          .map((e, idx) => (
+            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
+            <Fragment key={e.id}>
+              {!!idx && <Divider horizontalInset />}
+              <EventItem key={e.id} event={e} onSelect={onSelect} />
+            </Fragment>
+          ))}
+      </List.Section>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-  list: {
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  item: {
-    padding: 10,
-    width: Dimensions.get('window').width / 4,
-    alignItems: 'center',
-  },
-  label: { padding: 10 },
-});
