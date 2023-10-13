8ac918dc89a696994740746ad32fed25184afe97
Joseph Hale <me@jhale.dev>
2023-10-12 23:09:07 -0700

    feat(examples): Add `RaisedCard` wrapper
    This wrapper makes it easier to see each example component in isolation.

--- src/ui/examples/wrappers.tsx
+++ src/ui/examples/wrappers.tsx
@@ -5,6 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import { View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 
 type Wrapper = ({ children }: { children: React.ReactNode }) => JSX.Element;
 
@@ -20,6 +21,28 @@ function Centered({ children }: { children: React.ReactNode }) {
   );
 }
 
-const wrappers: Wrapper[] = [Centered];
+function RaisedCard({ children }: { children: React.ReactNode }) {
+  const theme = useTheme();
+  return (
+    <View
+      style={{
+        flex: 1,
+        margin: 20,
+        padding: 10,
+        backgroundColor: theme.colors.background,
+        borderRadius: 12,
+        // iOS
+        shadowColor: "#fff",
+        shadowOpacity: 0.3,
+        shadowRadius: 12,
+        // Android
+        elevation: 10
+      }}>
+      {children}
+    </View>
+  );
+}
+
+const wrappers: Wrapper[] = [Centered, RaisedCard];
 
 export { wrappers, Centered };
