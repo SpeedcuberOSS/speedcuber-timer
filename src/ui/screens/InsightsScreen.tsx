6b2077ea215714b4183e0e8e0feeb3bd26560cfd
Joseph Hale <me@jhale.dev>
2023-10-13 00:34:45 -0700

    refactor(insights): Extract Averages Table
    The DataTable was written in-line on the Screen.
    
    Extracting it makes the Screen's code much easier to read and sets me up
    for the many DataTable-specific adjustments I'll make next.

--- src/ui/screens/InsightsScreen.tsx
+++ src/ui/screens/InsightsScreen.tsx
@@ -4,53 +4,60 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-
+import { Attempt } from '../../lib/stif/wrappers';
 import { AttemptAnalytics } from '../../lib/analytics/AttemptAnalytics';
 import AttemptsChart from '../components/charts/AttemptsChart';
 import { DataTable } from 'react-native-paper';
+import SplitScreen from '../layouts/SplitScreen';
+import { StyleSheet } from 'react-native';
 import { TimerTabScreenProps } from '../navigation/types';
-import { useCompetitiveEvent } from '../hooks/useCompetitiveEvent';
 import { useAttempts } from '../../persistence/hooks';
-import { Attempt } from '../../lib/stif/wrappers';
+import { useCompetitiveEvent } from '../hooks/useCompetitiveEvent';
 
 type Props = TimerTabScreenProps<'Insights'>;
 
 export default function InsightsScreen(props: Props) {
   const [event] = useCompetitiveEvent();
-  const stifs = useAttempts({event, sortDirection: 'descending'});
+  const stifs = useAttempts({ event, sortDirection: 'descending' });
   const attempts: Attempt[] = [...stifs].map(a => new Attempt(a));
   const averages = [5, 12, 50, 100, 1000];
   return (
-    <SafeAreaView style={styles.container}>
+    <SplitScreen style={styles.container}>
       <AttemptsChart attempts={attempts} />
-      <View style={{ padding: 20 }}>
-        <DataTable>
-          <DataTable.Header>
-            <DataTable.Title>Type</DataTable.Title>
-            <DataTable.Title>Current</DataTable.Title>
-            <DataTable.Title>Record</DataTable.Title>
-          </DataTable.Header>
-        </DataTable>
-        {averages.map(count => (
-          <DataTable.Row key={count}>
-            <DataTable.Cell>{`Ao${count}`}</DataTable.Cell>
-            <DataTable.Cell>
-              {new AttemptAnalytics(attempts.slice(-count)).AoX(count) / 1000}
-            </DataTable.Cell>
-            <DataTable.Cell>
-              {Math.min(...new AttemptAnalytics(attempts).sliding.AoX(count)) /
-                1000}
-            </DataTable.Cell>
-          </DataTable.Row>
-        ))}
-      </View>
-    </SafeAreaView>
+      <AveragesTable averages={averages} attempts={attempts} />
+    </SplitScreen>
+  );
+}
+
+interface InsightsScreenProps {
+  averages: number[];
+  attempts: Attempt[];
+}
+
+export function AveragesTable({ averages, attempts }: InsightsScreenProps) {
+  return (
+    <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Type</DataTable.Title>
+        <DataTable.Title>Current</DataTable.Title>
+        <DataTable.Title>Record</DataTable.Title>
+      </DataTable.Header>
+      {averages.map(count => (
+        <DataTable.Row key={count}>
+          <DataTable.Cell>{`Ao${count}`}</DataTable.Cell>
+          <DataTable.Cell>
+            {new AttemptAnalytics(attempts.slice(-count)).AoX(count) / 1000}
+          </DataTable.Cell>
+          <DataTable.Cell>
+            {Math.min(...new AttemptAnalytics(attempts).sliding.AoX(count)) /
+              1000}
+          </DataTable.Cell>
+        </DataTable.Row>
+      ))}
+    </DataTable>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { margin: 10 },
 });
