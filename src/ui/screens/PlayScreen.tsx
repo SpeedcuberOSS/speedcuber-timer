f0b3b80c0de8c2b9de2f3391879d4318ce589748
Joseph Hale <me@jhale.dev>
2023-12-20 23:27:30 -0700

    build: Fix iOS deployment for 0.2.3
    Speedcuber Timer is now pending review on both Google Play and the App Store!

--- src/ui/screens/PlayScreen.tsx
+++ src/ui/screens/PlayScreen.tsx
@@ -4,25 +4,26 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { ATTEMPT_RELAY_234567 } from '../../lib/stif/demo';
+import { Attempt } from '../../lib/stif/wrappers';
+import AttemptPlayer from '../components/TwistyPlayer/AttemptPlayer';
 import { RootDrawerScreenProps } from '../navigation/types';
+import { STIF } from '../../lib/stif';
 import { SafeAreaView } from 'react-native';
-import ScramblingView from '../components/scrambles/ScramblingView';
-import { useState } from 'react';
 
 type Props = RootDrawerScreenProps<'Play'>;
 
+const attempt =
+  require('../../lib/recordings/__fixtures__/particula_3x3x3_attempt.json') as STIF.Attempt;
+
 export default function PlayScreen(props: Props) {
-  const [count, setCount] = useState(0);
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScramblingView
-        previousAttempt={ATTEMPT_RELAY_234567}
-        onPress={(scrambles) => {
-          setCount(count + 1);
-          console.log(scrambles, count, 'pressed');
-        }}
-      />
+      {/* <Button
+        onPress={() => console.log(JSON.stringify(getLibrary().getAll()))}>
+        Print Attempt Library
+      </Button>
+      */}
+      <AttemptPlayer attempt={new Attempt(attempt)} />
     </SafeAreaView>
   );
 }
