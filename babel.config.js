58369768bbb11226cc1be0e84cd8d428391105f3
Joseph Hale <me@jhale.dev>
2024-12-10 00:19:26 -0700

    chore: Upgrade `react-native-vector-icons` to New Architecture
    The best part is that this upgrade **simplifies** my code and processes.
    
    I no longer have to do the awkward manual linking steps in
    Android and iOS. In fact, because `react-native-vector-icons` now
    installs all fonts in `assets/fonts`, it also handles my custom fonts!
    
    This commit also removes all the unused icon packs. My hope is
    that will contribute to a smaller bundle size.
    
    The one downside is that I have to add a bunch of mocks for my tests
    to pass again. Hopefully that will be fixed more cleanly in the future.
     - See: https://github.com/oblador/react-native-vector-icons/issues/1681#issuecomment-2585560725

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
+const overrideReactNativePaperIcons = [
+  'module-resolver',
+  {
+    alias: {
+      'react-native-vector-icons/MaterialCommunityIcons': '@react-native-vector-icons/material-design-icons',
+    },
+  },
+]
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [overrideReactNativePaperIcons, 'react-native-reanimated/plugin'],
   env: {
     production: {
       plugins: ['react-native-paper/babel', 'transform-remove-console'],
